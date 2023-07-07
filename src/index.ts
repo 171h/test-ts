3667c10348521424a4b4d9228273763ac06987fd
H171 <1091088708@qq.com>
2023-07-07 22:07:23 +0800

    chore: add `docx`

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 // import './set'
 // import { testDebounce } from './debounce'
 // import { testDebounce as testDebounce2 } from './debounce2'
-import './untyped/demo'
+// import './untyped/demo'
+import './docx/demo'
 
 // testDebounce()
 // testDebounce2()
