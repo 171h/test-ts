64cd58921d92678920083137b835ced9a2fd2abf
H171 <1091088708@qq.com>
2023-07-19 22:20:12 +0800

    chore: update

--- src/docx/demo.ts
+++ src/docx/demo.ts
@@ -1,5 +1,5 @@
 import * as fs from 'node:fs'
-import { Document, Packer, Paragraph, TextRun } from 'docx'
+import { BorderStyle, Document, HeadingLevel, IBorderOptions, Packer, Paragraph, SectionType, SymbolRun, TextRun } from 'docx'
 
 // Documents contain sections, you can have multiple sections per document, go here to learn more about sections
 // This simple example will only contain one section
@@ -17,13 +17,19 @@ const doc = new Document({
       value: '1.0.0',
     },
   ],
+  background: {
+    // color: '#FF0000',
+  },
   sections: [
     {
-      properties: {},
+      properties: {
+        type: SectionType.ODD_PAGE,
+
+      },
       children: [
         new Paragraph({
           children: [
-            new TextRun('Hello World'),
+            new TextRun('Hello World1'),
             new TextRun({
               text: 'Foo Bar',
               bold: true,
@@ -32,7 +38,48 @@ const doc = new Document({
               text: '\tGithub is the best',
               bold: true,
             }),
+            new SymbolRun('F071'),
           ],
+          heading: HeadingLevel.HEADING_1,
+          border: {
+            bottom: {
+              style: BorderStyle.DASHED,
+              size: 10,
+              color: '#FF0000',
+            },
+          },
+          widowControl: true,
+          spacing: {
+            before: 100,
+            after: 100,
+          },
+        }),
+        new Paragraph({
+          children: [
+            new TextRun('Hello World1'),
+            new TextRun({
+              text: 'Foo Bar',
+              bold: true,
+            }),
+            new TextRun({
+              text: '\tGithub is the best',
+              bold: true,
+            }),
+            new SymbolRun('F071'),
+          ],
+          heading: HeadingLevel.HEADING_1,
+          border: {
+            bottom: {
+              style: BorderStyle.DASHED,
+              size: 10,
+              color: '#FF0000',
+            },
+          },
+          widowControl: true,
+          spacing: {
+            before: 100,
+            after: 100,
+          },
         }),
       ],
     },
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
 // import { testDebounce as testDebounce2 } from './debounce2'
 // import './untyped/demo'
 
-import './docx/demo'
+import './docx/demo3'
 
 // main()
 
