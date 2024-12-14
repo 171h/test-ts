9f5c31ffc70e7258dbad8d130e6d871166083ff5
171H <1091088708@qq.com>
2024-12-14 22:26:05 +0800

    chore: test class props in constructor

--- src/class/hello-world.ts
+++ src/class/hello-world.ts
@@ -96,3 +96,23 @@ export function useFileSystemAccess(options: { name: string }): string
 export function useFileSystemAccess(options = {}): string {
   return ''
 }
+
+console.log('------------ClassService-------------')
+class ClassService {
+  constructor(
+    private readonly _name: string,
+    private readonly _age: number
+  ) {}
+
+  public get name(): string {
+    return this._name
+  }
+
+  public get age(): number {
+    return this._age
+  }
+}
+
+const cs = new ClassService('hwj', 32)
+console.log(cs.name)
+console.log(cs.age)
--- src/index.ts
+++ src/index.ts
@@ -105,7 +105,7 @@ import { Logger } from '@171h/log'
 // import './fflate-unzip2/demo'
 // import './fflate/unzipmpp'
 
-// import './class/hello-world'
+import './class/hello-world'
 
 // import './mpxrs/demo'
 
@@ -117,4 +117,5 @@ import { Logger } from '@171h/log'
 // import './minimist'
 // import './path/__dirname'
 // import './class/hello-world'
-import './decorator'
+// import './decorator'
+
